42b697ee6ec9273f30885b209b1d4d3a792134f5
skmeetaali <mitalikamble5@gmail.com>
2026-06-20 16:27:11 +0530

    created Products app models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from accounts.models import Seller
 
 # Create your models here.
+class Products(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    product_id = models.UUIDField(primary_key=True)
+    product_name = models.CharField(max_length=128, unique=True, blank = True, null = True)
+    brancd = models.CharField(max_length=64, blank = True, null = True)
+    price = models.FloatField(blank=True)
+    description = models.CharField(max_length=1024)
+    category = models.CharField(max_length= 64, blank = True, null = True)
+    hsn_code = models.ForeignKey("HSNCode",on_delete=models.PROTECT)    
+    
+class Product_picturs(models.Model):
+    product_id = models.ForeignKey(Products, primary_key=True)
+    picture = models.ImageField()
+    is_default = models.BooleanField(default=False)
+    
+
+class HSNCode(models.Model):
+    code = models.CharField(max_length=8)
+    description = models.TextField()
+
+class GSTRate(models.Model):
+    hsn_code = models.ForeignKey(HSNCode)
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+    effective_from = models.DateField()
+    effective_to = models.DateField(null=True)
+    
