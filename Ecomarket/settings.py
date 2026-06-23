a5f8bec55643d22fbc9acdb004aec7bec588aee4
skmeetaali <mitalikamble5@gmail.com>
2026-06-23 12:57:04 +0530

    created adress model

--- Ecomarket/settings.py
+++ Ecomarket/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'orders',
+    'addresses',
 ]
 
 MIDDLEWARE = [
--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import Buyer
 
 # Create your models here.
+class address(models.Model):
+    customer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    house = models.CharField(max_length=64, null=False)
+    city = models.CharField(max_length=64, null=False)
+    state = models.CharField(max_length=64, null=False)
+    zipcode = models.CharField(max_length=64, null=False)
