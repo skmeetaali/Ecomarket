2ea01662e268e36267cc00eeb66099781a609b71
skmeetaali <mitalikamble5@gmail.com>
2026-06-23 11:41:40 +0530

    created Order and OrderItem models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import Buyer
+from products.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    customer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    transaction_id = models.CharField(max_length=200, null=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    
