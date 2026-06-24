d8fcd15ad158bebc1fc2f9ff62abb1796b471951
skmeetaali <mitalikamble5@gmail.com>
2026-06-24 08:24:28 +0530

    created payment model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from orders.models import Order
 
 # Create your models here.
+class Payment(models.Model):
+            
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    payment_provider = models.CharField(max_length=128)
+    transaction_id = models.CharField(max_length=200)
+    amount = models.FloatField(blank=True, null=False)
+    payment_status = models.CharField(max_length=64)
