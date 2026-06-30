5a06f33a22198c2923c731624324f60a141b3c6a
skmeetaali <mitalikamble5@gmail.com>
2026-06-30 23:55:43 +0530

    buyer profile and login

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,3 +6,25 @@ class UserSigninForm(UserCreationForm):
         model = User 
         fields = ["username", "email", "role", "password1", "password2"]
         
+        
+from django import forms
+from .models import Buyer
+
+class BuyerSignupForm(forms.ModelForm):
+    class Meta:
+        model = Buyer
+        fields = ["birth_date", "profile_picture"]
+        
+        widgets = {
+            "birth_date": forms.DateInput(attrs={"type": "date"}),
+        }
+        
+class LoginForm(forms.Form):
+    identifier = forms.CharField(
+        label="Username or Email"
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput
+    )
+    
+    
